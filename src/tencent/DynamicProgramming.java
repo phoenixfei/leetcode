f185ac390336e1cfc79248df68042d8704f04fd9
phe <369526049@qq.com>
2019-04-22 14:55:48 +0800

    test adding win10 files

--- src/tencent/DynamicProgramming.java
+++ src/tencent/DynamicProgramming.java
@@ -4,7 +4,7 @@ package tencent;
  * DynamicProgramming
  */
 public class DynamicProgramming {
-
+    // https://leetcode.com/problems/climbing-stairs/
     public int climbStairs(int n) {
         if(n == 1 || n == 2) return n;
         int[] arr = new int[n+1];
@@ -19,5 +19,20 @@ public class DynamicProgramming {
             arr[n] = climb(n-1, arr) + climb(n-2, arr);
             return arr[n];
         }
+    }
+
+    // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    // 注意if else的用法
+    public int maxProfit(int[] prices) {
+        int maxVal = 0;
+        int buyPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if(prices[i] < buyPrice){
+                buyPrice = prices[i];
+            }else if(prices[i] - buyPrice > maxVal){
+                maxVal = prices[i] - buyPrice;
+            }
+        }
+        return maxVal;
     }
 }
