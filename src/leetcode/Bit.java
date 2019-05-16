904177c568399c31ca1c676597835949c4133f76
phoenix_yxf <pheonix_yxf@outlook.com>
2019-05-16 11:00:42 +0800

    add code bit treemap 0516

---
+++ src/leetcode/Bit.java
@@ -0,0 +1,114 @@
+package leetcode;
+
+/**
+ * Bit 位操作 https://www.jianshu.com/p/b677858bc085 基本的位操作有 与 、 或 、 异或 、 取反 、 左移 、
+ * 右移 共 6 种。 & 与 两个位都为1时，结果才为1. | 或 两个位都为0时，结果才为0. ^ 异或 两个位相同时为0，相异为1.
+ * 任何数与0异或不变，与1异或相当于取反 ~ 取反 0变1，1变0. << 左移 各二进位全部左移若干位，高位丢弃，低位补0. >> 右移
+ * 各二进制全部右移若干位，左边用原有标志位补充，右边超出部分舍弃. >>> 无符号右移 左边部分总是以0补充，右边部分舍弃.
+ * 
+ * 注意 1、位操作只能用于 整形 数据。 2、位操作的优先级比较低，因此尽量使用 扩号 来确保运算顺序。 
+ * 
+ * 位操作的一些小技巧 1、判断奇偶 2、交换两数 3、变换符号 4、求绝对值
+ */
+public class Bit {
+    // 判断奇偶
+    public static boolean isOven(int i) {
+        if ((i & 1) == 0)
+            return true; // a % 2 == 0
+        else
+            return false;
+    }
+
+    // 交换两数
+    public static void swap(int a, int b) {
+        System.out.println("a = " + a + "\tb = " + b);
+        a = a ^ b; // a ^= b
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("a = " + a + "\tb = " + b);
+    }
+
+    /**
+     * 变换符号：正变负，负变正 根据正数与负数在计算机中的存储方式，可知：交换符号可通过 按位取反，末位加一
+     */
+    public static int exchangeSymbol(int a) {
+        return (~a) + 1;
+    }
+
+    /**
+     * 求绝对值 首先，通过 a >> 31 取得符号位，再根据交换符号中的方法求绝对值
+     */
+    public static int abs(int a) {
+        int sign = a >> 31;
+        return sign == 0 ? a : ~a + 1;
+    }
+
+    /**
+     * 求绝对值 利用异或 首先，通过 a >> 31 取得符号位，0或者-1。再根据a与sign的异或值再减去sign。
+     * 如果sign为0，值还为a；如果sign为-1，相当于按位取反(符号位参与运算)，末位加1.
+     */
+    public static int absXOR(int a) {
+        int sign = a >> 31;
+        return (a ^ sign) - sign;
+    }
+
+    /**
+     * 其它小技巧
+     * 
+     */
+    public static void others() {
+        System.out.println("int型最大值：");
+        System.out.println(~(1 << 31));
+        System.out.println((1 << 31) - 1);
+
+        System.out.println("int型最小值：");
+        System.out.println((1 << 31));
+        System.out.println((1 << -1));
+
+        System.out.println("long型最大值：");
+        System.out.println(~((long) 1 << 31));
+
+        // 乘以2运算
+        System.out.println(10 << 1);
+
+        // 除以2运算(负奇数的运算不可用)
+        System.out.println(10 >> 1);
+
+        // 乘以2的m次方
+        System.out.println(10 << 2);
+
+        // 除以2的m次方
+        System.out.println(16 >> 2);
+
+        int n = 0, m = 6;
+        // 从低位到高位.将n的第m位置为1
+        // 将1左移m-1位找到第m位，得到000...1...000, n在和这个数做或运算
+        System.out.println(n | (1 << (m - 1)));
+
+        // 将1左移m-1位找到第m位，取反后变成111...0...1111, n再和这个数做与运算
+        System.out.println(n & ~(0 << (m - 1)));
+    }
+
+    public static void main(String[] args) {
+        // 计算机中的原码反码和补码 https://blog.csdn.net/code_caq/article/details/68925861
+        System.out.println(Integer.toBinaryString(Integer.MIN_VALUE));
+        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
+        // System.out.println("判断奇偶：");
+        // for (int i = -5; i < 5; i++) {
+        //     System.out.println(i + "是偶数吗？" + isOven(i));
+        // }
+        // System.out.println("交换两数：");
+        // swap(5, 2);
+        // System.out.println("交换两数的符号：");
+        // for (int i = -5; i < 5; i++) {
+        //     System.out.println(i + "交换符号后为：" + exchangeSymbol(i));
+        // }
+        // System.out.println("求绝对值：");
+        // for (int i = -5; i < 5; i++) {
+        //     System.out.println(i + "的绝对值为：" + abs(i));
+        // }
+        // for (int i = -5; i < 5; i++) {
+        //     System.out.println(i + "的绝对值为：" + absXOR(i));
+        // }
+    }
+}
--- src/leetcode/TreeNodeS.java
+++ src/leetcode/TreeNodeS.java
@@ -80,9 +80,68 @@ public class TreeNodeS {
         return ret;
     }
 
-    // https://leetcode.com/problems/validate-binary-search-tree/
+    // 树的深度 https://leetcode.com/problems/maximum-depth-of-binary-tree/
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
     /**
-     * 递归确定 二叉搜索树 1. 节点的左子树包含小于当前节点的数； 2. 节点的右子树包含大于当前节点的数； 3. 节点的左右子树自身必须也是二叉搜索树。
+     * https://leetcode.com/problems/balanced-binary-tree/ 给定一个二叉树，判断它是否是高度平衡的二叉树。
+     * 本题中，一棵高度平衡二叉树定义为：一个二叉树每个节点的左右两个子树的高度差的绝对值不超过1。
+     */
+    public boolean isBalanced(TreeNode root) {
+        maxBalancedDepth(root);
+        return result;
+    }
+
+    private boolean result = true;
+
+    private int maxBalancedDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int l = maxBalancedDepth(root.left);
+        int r = maxBalancedDepth(root.right);
+        if (Math.abs(l - r) > 1)
+            result = false;
+        return 1 + Math.max(l, r);
+    }
+
+    /**
+     * https://leetcode.com/problems/diameter-of-binary-tree/
+     * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过根结点。
+     */
+    private int maxLen = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDiameter(root);
+        return maxLen;
+    }
+
+    private int maxDiameter(TreeNode root) {
+        if (root == null)
+            return 0;
+        int l = maxDiameter(root.left);
+        int r = maxDiameter(root.right);
+        // 此时，l,r还没有+1，所有l+r不需要-2
+        maxLen = Math.max(l + r, maxLen);
+        return Math.max(l, r) + 1;
+    }
+
+    // 翻转二叉树 https://leetcode.com/problems/invert-binary-tree/
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+
+    /**
+     * https://leetcode.com/problems/validate-binary-search-tree/ 递归确定 二叉搜索树 1.
+     * 节点的左子树包含小于当前节点的数； 2. 节点的右子树包含大于当前节点的数； 3. 节点的左右子树自身必须也是二叉搜索树。
      */
     public boolean isValidBST(TreeNode root) {
         if (root == null)
